1e67ca2f7ab1760e70370aa17495d85f86062155
Jakob spiel <jako5457@gmail.com>
2025-02-11 08:39:51 +0100

    Cascading parameters

---
+++ AdvancedComponents/Program.cs
@@ -0,0 +1,14 @@
+using AdvancedComponents;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Cascading Value fra toppen
+builder.Services.AddCascadingValue("Theme", sp => "Light");
+
+await builder.Build().RunAsync();
